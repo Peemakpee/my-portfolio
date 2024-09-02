6156b1b39cff631dd22f8d38192d01abac29b067
Peemak <cabanesmcphy123@gmail.com>
2024-09-02 16:53:46 +0800

    added project

--- mc-portfolio/stores/useProjectStore.js
+++ mc-portfolio/stores/useProjectStore.js
@@ -9,45 +9,6 @@ export const useProjectStore = defineStore('projectStore', {
         projectsDescription: 'Description of your projects',
         projects: [
             {
-                id: "f6b966b7-45c2-48c5-ab26-91f99984d50e",
-                title: "MedCare",
-                category: "Desktop Application",
-                img: "projects/vbnet/med_care3.png",
-                publishDate: "June 5, 2024",
-                tag: "Desktop/VB.NET",
-                techTitle: "Tools & Technologies",
-                detailsTitle: "About Project",
-                projectImages: [
-                    {
-                        id: uuidv4(),
-                        title: "Desktop Application",
-                        img: "/projects/vbnet/med_care1.png",
-                    },
-                    {
-                        id: uuidv4(),
-                        title: "Desktop Application",
-                        img: "/projects/vbnet/med_care2.png",
-                    },
-                    {
-                        id: uuidv4(),
-                        title: "Desktop Application",
-                        img: "/projects/vbnet/med_care4.png",
-                    },
-                ],
-                technologies: [
-                    "VB.NET",
-                    "MYSQL",
-                    "Visual Studio",
-                ],
-                projectDetails: [
-                    {
-                        id: uuidv4(),
-                        details:
-                            "MedCare is a desktop application designed for a medical facility to streamline the storage of patient data and appointments. It implements a stack data structure using the Last-In-First-Out (LIFO) method, ensuring that the latest appointment data is displayed on top of the table view, enabling efficient prioritization.",
-                    },
-                ],
-            },
-            {
                 id: "9c2f5137-548d-4023-afe4-0cd09b6d791f",
                 title: "BTr Status Tagging System",
                 category: "Web Application",
@@ -85,6 +46,45 @@ export const useProjectStore = defineStore('projectStore', {
                         id: uuidv4(),
                         details:
                             "The BTr Status Tagging System is a simple web-based application developed for the Bureau of the Treasury, RXI, as part of my internship. It features basic data visualization using ChartJS and is used to monitor and manage the status of incoming and outgoing documents.",
+                    },
+                ],
+            },
+            {
+                id: "f6b966b7-45c2-48c5-ab26-91f99984d50e",
+                title: "MedCare",
+                category: "Desktop Application",
+                img: "projects/vbnet/med_care3.png",
+                publishDate: "June 5, 2024",
+                tag: "Desktop/VB.NET",
+                techTitle: "Tools & Technologies",
+                detailsTitle: "About Project",
+                projectImages: [
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/med_care1.png",
+                    },
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/med_care2.png",
+                    },
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/med_care4.png",
+                    },
+                ],
+                technologies: [
+                    "VB.NET",
+                    "MYSQL",
+                    "Visual Studio",
+                ],
+                projectDetails: [
+                    {
+                        id: uuidv4(),
+                        details:
+                            "MedCare is a desktop application designed for a medical facility to streamline the storage of patient data and appointments. It implements a stack data structure using the Last-In-First-Out (LIFO) method, ensuring that the latest appointment data is displayed on top of the table view, enabling efficient prioritization.",
                     },
                 ],
             },
@@ -128,7 +128,7 @@ export const useProjectStore = defineStore('projectStore', {
                 ],
             },
             {
-                id: "0a3576bb-585c-4506-99d4-afebc6fd88b7",
+                id: "b9269f51-eb23-4e66-8b9b-c8ed760e1562",
                 title: "Bible Bliss",
                 category: "Web Application",
                 img: "projects/laravel/bible-bliss1.png",
@@ -140,29 +140,68 @@ export const useProjectStore = defineStore('projectStore', {
                     {
                         id: uuidv4(),
                         title: "Desktop Application",
-                        img: "/projects/flutter/bible-bliss1.png",
+                        img: "/projects/laravel/bible-bliss1.png",
                     },
                     {
                         id: uuidv4(),
                         title: "Desktop Application",
-                        img: "/projects/flutter/bible-bliss2.png",
+                        img: "/projects/laravel/bible-bliss2.png",
                     },
                     {
                         id: uuidv4(),
                         title: "Desktop Application",
-                        img: "/projects/flutter/bible-bliss1.png",
+                        img: "/projects/laravel/bible-bliss1.png",
                     },
                 ],
                 technologies: [
-                    "Flutter",
-                    "Firebase",
-                    "Android Studio",
+                    "Laravel",
+                    "CSS",
+                    "Rapid API",
                 ],
                 projectDetails: [
                     {
                         id: uuidv4(),
                         details:
-                            "Diwang, meaning 'Celebration' in Filipino, is a mobile application developed in Flutter to connect event organizers with potential clients. With the surge in events post-pandemic, Diwang aims to simplify the process of finding and booking services like catering, decorations, drinks, and other essential event needs, making it easier for organizers and clients to collaborate.",
+                            "Bible Bliss is a Laravel-powered web application that serves as a Bible content tool with dynamic content delivery, focusing on retrieving and displaying Bible verses from an external API based on user searches and daily verse updates.",
+                    },
+                ],
+            },
+            {
+                id: uuidv4(),
+                title: "SkyNet",
+                category: "Desktop Application",
+                img: "projects/vbnet/skynet1.png",
+                publishDate: "October 5, 2023",
+                tag: "Desktop/VB.NET",
+                techTitle: "Tools & Technologies",
+                detailsTitle: "About Project",
+                projectImages: [
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/skynet2.png",
+                    },
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/skynet3.png",
+                    },
+                    {
+                        id: uuidv4(),
+                        title: "Desktop Application",
+                        img: "/projects/vbnet/skynet4.png",
+                    },
+                ],
+                technologies: [
+                    "VB.NET",
+                    "MYSQL",
+                    "Visual Studio",
+                ],
+                projectDetails: [
+                    {
+                        id: uuidv4(),
+                        details:
+                            "Bible Bliss is a Laravel-powered web application that serves as a Bible content tool with dynamic content delivery, focusing on retrieving and displaying Bible verses from an external API based on user searches and daily verse updates.",
                     },
                 ],
             },
